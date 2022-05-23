ff043d51a3034c7014fad759d622c12ac62cde78
Steven Borkman <steven.borkman@unity3d.com>
2022-05-23 11:00:08 -0400

    Getting rid of the cruft that comes with a python project from template

--- template/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Python template project."""
-import importlib.metadata
-import logging
-
-from . import module  # noqa: F401
-
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
-
-__version__ = importlib.metadata.version("python-template")
